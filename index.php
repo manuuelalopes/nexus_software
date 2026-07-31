68ce6706cf39f7018732d3d52e2f9f6825a5701e
manuuelalopes <manulopes3301@gmail.com>
2026-07-31 16:29:30 -0300

    aprendendo html em php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- titulo da aba do navegador -->
+    <title>nexus software</title> 
+</head>
+<body>
+    <!-- primeiro titulo do site -->
+    <h1>nexus software</h1> 
+    <?php
+    // "prints" para mostrar o texto na tela e "br" para quebrar a linha
+    echo "<h2><strong> nexus software é uma empresa dedicada a fornecer soluções tecnológicas de qualidade.</strong></h2>";?> <br>
+    <?php
+    echo "<h3><strong> foco em excelência e inovação.</strong></h3>";?> <br>
+    <?php
+    echo "<h4><strong> Fale Conosco no WhatsApp: 19 99999-9999</strong></h4>";?> <br>
+</body>
+</html>
